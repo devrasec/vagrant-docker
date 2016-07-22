8c275b5304eda22335a55757a5856cacdb1d1e6f
Luis Contreras <one.tmac@gmail.com>
2016-07-22 16:44:32 -0400

    Setup the daemon to listen on tcp port and linux socket

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Change the IP for the vm.
+export DOCKER_HOST="tcp://192.168.10.20:2375"
--- provision/shell/docker_bind_port.sh
+++ provision/shell/docker_bind_port.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "Binding Docker daemon to port 2375..."
-if grep -Fxq 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' /etc/default/docker
+
+DOCKER_OPTS="DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\""
+
+if grep -Fxq $DOCKER_OPTS /etc/default/docker
 then
     echo "Already configured!"
 else
-    echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
+    echo $DOCKER_OPTS > /etc/default/docker
     service docker restart
     echo "Done!"
 fi
